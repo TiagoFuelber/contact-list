6c2f8cc3a28c0d588255839b24abee0b7155c489
TiagoFuelber <tfuelber@gmail.com>
2022-12-08 18:20:02 -0300

    feat: Add env variables
    * Added env variables to hide CLIENT_ID

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    CLIENT_ID: process.env.CLIENT_ID,
+    REDIRECT_URI: process.env.REDIRECT_URI,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
